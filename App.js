d3e516bab4fdfce8b6d23bdcabc28facdd957f18
Alexey Egoshin <alexeyegoshin0403@mail.ru>
2023-03-23 13:18:33 +0700

    Create start page

--- App.js
+++ App.js
@@ -1,14 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import BackButton from './src/components/BackButton';
-import Group from "./src/components/Group";
-import SearchField from "./src/components/SearchField";
-import GroupSearch from './src/views/GroupSearch';
+import StartPage from "./src/views/StartPage";
+import GroupSearch from "./src/views/GroupSearch";
 
 export default function App() {
   return (
     <View>
-      <GroupSearch/>
+      {/*<GroupSearch/>*/}
+        <StartPage />
     </View>
   );
 }
