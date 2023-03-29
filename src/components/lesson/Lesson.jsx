c2384ab9a828ef0d36712cbbe9da7ac4a3ce5f72
Alexey Egoshin <alexeyegoshin0403@mail.ru>
2023-03-29 19:33:30 +0700

    Create lesson component dynamic

--- src/components/lesson/Lesson.jsx
+++ src/components/lesson/Lesson.jsx
@@ -2,14 +2,24 @@ import { View, Text } from "react-native";
 
 import Styles from "./lesson.scss";
 
-const Lesson = ({ time, title, teacher, room }) => {
-    return(
-        <View style={Styles.lesson__wrapper}>
-            <View style={Styles.lesson__wrapper_container}>
-                <Text style={Styles.lesson__wrapper_container_time}>{time}</Text>
-                <Text style={Styles.lesson__wrapper_container_title}>{title}</Text>
-                <Text style={Styles.lesson__wrapper_container_info}>{teacher}</Text>
-                <Text style={Styles.lesson__wrapper_container_info}>{room}</Text>
+const Lesson = ({ time, title, teacher, room, isActive }) => {
+    console.log(isActive);
+    return (
+        <View style={Styles.content}>
+            <View style={Styles.content__timeline} >
+                <View style={Styles.content__timeline_top} />
+                <View
+                    style={[Styles.content__timeline_point, isActive ? Styles.content__timeline_point_active : Styles.content__timeline_point_inactive]}
+                />
+                <View style={Styles.content__timeline_bottom} />
+            </View>
+            <View style={Styles.lesson__wrapper}>
+                <View style={Styles.lesson__wrapper_container}>
+                    <Text style={Styles.lesson__wrapper_container_time}>{time}</Text>
+                    <Text style={Styles.lesson__wrapper_container_title}>{title}</Text>
+                    <Text style={Styles.lesson__wrapper_container_info}>{teacher}</Text>
+                    <Text style={Styles.lesson__wrapper_container_info}>{room}</Text>
+                </View>
             </View>
         </View>
     );
