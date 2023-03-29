46d1c586476afc00197daa2742b0f1451665b968
Alexey Egoshin <alexeyegoshin0403@mail.ru>
2023-03-29 19:33:55 +0700

    Some fixes

--- App.js
+++ App.js
@@ -6,9 +6,12 @@ import TabsBar from "./src/components/tabsBar/TabsBar";
 import Navigator from "./src/routes/navigator";
 import GroupSearch from "./src/views/groupSearchPage/GroupSearch";
 import SettingsView from "./src/views/settingsPage/SettingsView";
+import SchedulePage from "./src/views/schedulePage/SchedulePage";
 
 export default function App() {
     return (
-        <Lesson time={"11:40 - 13:15"} title={"Программирование мобильных устройств (лк)"} teacher={"Нечта И. В."} room={"а. 202 (К.1)"}/>
+        // <Lesson time={"11:40 - 13:15"} title={"Программирование мобильных устройств (лк)"} teacher={"Нечта И. В."} room={"а. 202 (К.1)"}/>
+        // <Calendar/>
+        <SchedulePage></SchedulePage>
     );
 }
