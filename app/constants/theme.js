2186a520ab27b9ac9f32a908275a44ed12bffbcd
Alexey Egoshin <alexeyegoshin0403@mail.ru>
2023-08-24 15:48:25 +0700

    Added a file with the application colors

---
+++ app/constants/theme.js
@@ -0,0 +1,14 @@
+const colors = {
+  light: {
+    primary: '#187DC2',
+    secondary: '#FFFFFF',
+    text: '#000000',
+  },
+  dark: {
+    primary: '#506699',
+    secondary: '#1C1B1F',
+    text: '#DEDEDE',
+  },
+};
+
+export { colors };
