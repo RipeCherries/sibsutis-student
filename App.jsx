d7def53158b0cb2708c1df634f1c4cb1ee355cd6
Alexey Egoshin <alexeyegoshin0403@mail.ru>
2023-08-25 20:07:13 +0700

    Fix codestyle and add theme support

--- App.jsx
+++ App.jsx
@@ -1,34 +1,47 @@
-import React, {useCallback} from 'react';
-import {View} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import TabNavigation from './app/navigation/TabNavigation';
-import {useFonts} from "expo-font";
+import React, { useCallback, useMemo } from 'react';
+import { View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
+import TabNavigation from './app/navigation/TabNavigation';
+import { ThemeContext } from './app/context/ThemeContext';
+import { useTheme } from './app/hooks/useTheme';
 
 SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-    const [fontsLoaded] = useFonts({
-        'Montserrat-Regular': require('./assets/fonts/Montserrat-Regular.ttf'),         // 400
-        'Montserrat-Bold': require('./assets/fonts/Montserrat-Bold.ttf')                // 700
-    });
+  const [fontsLoaded] = useFonts({
+    'Montserrat-Regular': require('./assets/fonts/Montserrat-Regular.ttf'), // 400
+    'Montserrat-Bold': require('./assets/fonts/Montserrat-Bold.ttf'), // 700
+  });
 
-    const onLayoutRootView = useCallback(async () => {
-        if (fontsLoaded) {
-            await SplashScreen.hideAsync();
-        }
-    }, [fontsLoaded]);
+  const {
+    theme,
+    toggleTheme,
+  } = useTheme();
 
-    if (!fontsLoaded) {
-        return null;
+  const themeContextValue = useMemo(() => ({
+    theme,
+    toggleTheme,
+  }), [theme, toggleTheme]);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
     }
+  }, [fontsLoaded]);
 
+  if (!fontsLoaded) {
+    return null;
+  }
 
-    return (
-        <View style={{flex: 1}} onLayout={onLayoutRootView}>
-            <NavigationContainer>
-                <TabNavigation/>
-            </NavigationContainer>
-        </View>
-    );
+  return (
+    <ThemeContext.Provider value={themeContextValue}>
+      <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+        <NavigationContainer>
+          <TabNavigation />
+        </NavigationContainer>
+      </View>
+    </ThemeContext.Provider>
+  );
 }
