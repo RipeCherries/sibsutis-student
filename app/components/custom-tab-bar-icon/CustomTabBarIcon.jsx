1eb284e4928c7e83ead87bf3dec7b32a5db1539f
Alexey Egoshin <alexeyegoshin0403@mail.ru>
2023-08-25 15:23:53 +0700

    Fix codestyle

--- app/components/custom-tab-bar-icon/CustomTabBarIcon.jsx
+++ app/components/custom-tab-bar-icon/CustomTabBarIcon.jsx
@@ -1,27 +1,26 @@
-import React, { useContext } from 'react';
+import React, {useContext} from 'react';
 import PropTypes from 'prop-types';
-import { View } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
-import { ThemeContext } from '../../context/ThemeContext';
+import {View} from 'react-native';
+import {Ionicons} from '@expo/vector-icons';
+import {ThemeContext} from '../../context/ThemeContext';
 
-import { colors } from '../../constants/theme';
+import {colors} from '../../constants/theme';
 import CustomTabBarIconStyles from './CustomTabBarIcon.styles';
 
-function CustomTabBarIcon({ focused, name }) {
-  const { theme } = useContext(ThemeContext);
-  const themeColors = colors[theme];
+function CustomTabBarIcon({focused, name}) {
+    const {theme} = useContext(ThemeContext);
+    const themeColors = colors[theme];
 
-  return (
-    <View
-      style={focused && [CustomTabBarIconStyles.customTabBarIconFocused, { backgroundColor: themeColors.secondary }]}
-    >
-      <Ionicons name={name} size={24} color={focused ? themeColors.primary : themeColors.secondary} />
-    </View>
-  );
+    return (
+        <View
+            style={focused && [CustomTabBarIconStyles.customTabBarIconFocused, {backgroundColor: themeColors.secondary}]}>
+            <Ionicons name={name} size={24} color={focused ? themeColors.primary : themeColors.secondary}/>
+        </View>
+    );
 }
 
 CustomTabBarIcon.propTypes = {
-  focused: PropTypes.bool.isRequired,
-  name: PropTypes.string.isRequired,
+    focused: PropTypes.bool.isRequired,
+    name: PropTypes.string.isRequired,
 };
 export default CustomTabBarIcon;
