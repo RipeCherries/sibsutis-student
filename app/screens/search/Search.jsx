07521d07dc40a0f3c90743c7c9989a0fcf20cf35
Alexey Egoshin <alexeyegoshin0403@mail.ru>
2023-09-08 15:58:10 +0700

    Made the UI of the screen with the search for groups

--- app/screens/search/Search.jsx
+++ app/screens/search/Search.jsx
@@ -1,20 +1,38 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, {useContext, useState} from 'react';
+import {
+  SafeAreaView, Text, FlatList, View,
+} from 'react-native';
 import { useSelector } from 'react-redux';
 import MainContainer from '../../components/main-container/MainContainer';
+import SearchField from '../../components/search-field/SearchField';
 
 import SearchStyles from './Search.styles';
+import GroupItem from '../../components/group-item/GroupItem';
+import {ThemeContext} from "../../context/ThemeContext";
+import {colors} from "../../constants/theme";
 
 function Search() {
-  const a = useSelector((store) => store.groups);
+  const { theme } = useContext(ThemeContext);
+  const themeColors = colors[theme];
+
+  const groups = useSelector((store) => store.groups);
+
+  const [searchData, setSearchData] = useState('');
+
+  const filteredGroups = groups.filter((item) => item.groupName.toLowerCase().includes(searchData.toLowerCase()));
+
   return (
     <MainContainer>
-      <Text>Search Screen</Text>
-      {
-            a.map((item) => (
-              <Text key={item.groupId}>{item.groupName}</Text>
-            ))
-        }
+      <View style={SearchStyles.container}>
+        <SearchField value={searchData} onChangeFunc={setSearchData} />
+        <View style={SearchStyles.groupsList}>
+          <FlatList
+            data={filteredGroups}
+            renderItem={({ item }) => <GroupItem groupName={item.groupName} />}
+            keyExtractor={(item) => item.groupId}
+          />
+        </View>
+      </View>
     </MainContainer>
   );
 }
--- app/screens/search/Search.styles.js
+++ app/screens/search/Search.styles.js
@@ -1,7 +1,15 @@
 import { StyleSheet } from 'react-native';
 
 const SearchStyles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'stretch',
+  },
+  groupsList: {
+    flex: 1,
+    marginVertical: 10,
+  },
 });
 
 export default SearchStyles;
