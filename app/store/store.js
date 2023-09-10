3caf65df13c70fd5f61e733f49bd31dfa4da7f96
Alexey Egoshin <alexeyegoshin0403@mail.ru>
2023-09-10 17:25:28 +0700

    Added information about the main group to the store

--- app/store/store.js
+++ app/store/store.js
@@ -2,9 +2,11 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import lastUpdateSlice from './lastUpdateSlice';
 import groupsSlice from './groupsSlice';
+import mainGroupSlice from './mainGroupSlice';
 
 const store = configureStore({
   reducer: {
+    mainGroup: mainGroupSlice,
     lastUpdate: lastUpdateSlice,
     groups: groupsSlice,
   },
