4091fba67346bf5180a3219bcd878fef004fab25
Alexey Egoshin <alexeyegoshin0403@mail.ru>
2023-09-11 20:13:12 +0700

    Remove handlePress logic

--- app/components/group-item/GroupItem.jsx
+++ app/components/group-item/GroupItem.jsx
@@ -1,24 +1,17 @@
 import React, { useContext } from 'react';
 import { Pressable, Text, View } from 'react-native';
 import { Feather } from '@expo/vector-icons';
-import {useNavigation} from "@react-navigation/native";
 
 import GroupItemStyles from './GroupItem.styles';
 import { colors } from '../../constants/theme';
 import { ThemeContext } from '../../context/ThemeContext';
 
-const GroupItem = React.memo(({ groupName }) => {
+const GroupItem = React.memo(({ groupName, onPress }) => {
   const { theme } = useContext(ThemeContext);
   const themeColors = colors[theme];
 
-  const navigation = useNavigation();
-
-  const handlePress = () => {
-      navigation.navigate('GroupSchedule');
-  }
-
   return (
-    <Pressable style={GroupItemStyles.container} onPress={handlePress}>
+    <Pressable style={GroupItemStyles.container} onPress={onPress}>
       <View style={GroupItemStyles.wrapper}>
         <Feather name="users" size={24} color={themeColors.primary} />
         <Text style={[GroupItemStyles.groupName, { color: themeColors.text }]}>{groupName}</Text>
