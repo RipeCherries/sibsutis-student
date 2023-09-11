aed9703440d14fcf98cd38b6b61e570099dddadd
Alexey Egoshin <alexeyegoshin0403@mail.ru>
2023-09-11 20:15:46 +0700

    Create full navigation in app

---
+++ app/navigation/Navigation.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { NavigationContainer } from '@react-navigation/native';
+import MainNavigation from './main-navigation/MainNavigation';
+import FirstRunNavigation from './first-run-navigation/FirstRunNavigation';
+
+function Navigation() {
+  const mainGroup = useSelector((store) => store.mainGroup);
+
+  return (
+    <NavigationContainer>
+      {
+                mainGroup ? <MainNavigation /> : <FirstRunNavigation />
+            }
+    </NavigationContainer>
+  );
+}
+
+export default Navigation;
