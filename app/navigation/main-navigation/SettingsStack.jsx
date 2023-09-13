52bb99cd0430588d4a1278e801a2e7db55c7cf74
Alexey Egoshin <alexeyegoshin0403@mail.ru>
2023-09-13 20:44:42 +0700

    Create a stack navigation for settings

---
+++ app/navigation/main-navigation/SettingsStack.jsx
@@ -0,0 +1,28 @@
+import { createStackNavigator } from '@react-navigation/stack';
+import { Settings, Search } from '../../screens';
+
+function SearchStack() {
+  const Stack = createStackNavigator();
+
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="SettingsStack"
+        component={Settings}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="SettingsSearch"
+        component={Search}
+        options={{
+          headerShown: false,
+        }}
+        initialParams={{ context: 'changeGroup' }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+export default SearchStack;
