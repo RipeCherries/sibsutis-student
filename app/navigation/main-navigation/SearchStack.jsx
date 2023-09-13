91b5f7de5d0a4c754aa1867aeb5a6c4942a1f7c1
Alexey Egoshin <alexeyegoshin0403@mail.ru>
2023-09-13 20:44:27 +0700

    Create stack navigation for search

---
+++ app/navigation/main-navigation/SearchStack.jsx
@@ -0,0 +1,27 @@
+import { createStackNavigator } from '@react-navigation/stack';
+import { Home, Search } from '../../screens';
+
+function SearchStack() {
+  const Stack = createStackNavigator();
+
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="SearchStack"
+        component={Search}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="GroupSchedule"
+        component={Home}
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+export default SearchStack;
