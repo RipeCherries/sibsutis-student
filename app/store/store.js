609c3beccc9d1187ea7c9006925dce9c60d7da50
Alexey Egoshin <alexeyegoshin0403@mail.ru>
2023-10-03 12:01:36 +0700

    Add new slices in store

--- app/store/store.js
+++ app/store/store.js
@@ -3,12 +3,16 @@ import { configureStore } from '@reduxjs/toolkit';
 import lastUpdateSlice from './lastUpdateSlice';
 import groupsSlice from './groupsSlice';
 import mainGroupSlice from './mainGroupSlice';
+import startOfSemesterSlice from './startOfSemesterSlice';
+import mainGroupLessonsSlice from './mainGroupLessonsSlice';
 
 const store = configureStore({
   reducer: {
     mainGroup: mainGroupSlice,
+    mainGroupLessons: mainGroupLessonsSlice,
     lastUpdate: lastUpdateSlice,
     groups: groupsSlice,
+    startOfSemester: startOfSemesterSlice,
   },
 });
 
