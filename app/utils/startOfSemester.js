3dee009feaf4450a2f11b50ee52cff73122d5cd0
Alexey Egoshin <alexeyegoshin0403@mail.ru>
2023-10-03 11:57:57 +0700

    Create funcs for working with start of semester

---
+++ app/utils/startOfSemester.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import { getDataFromStorage, saveDataInStorage } from '../store/asyncStorage';
+import { setStartOfSemester } from '../store/startOfSemesterSlice';
+
+export async function loadStartOfSemesterFromStorage(dispatch) {
+  try {
+    const storedStartOfSemester = await getDataFromStorage('startOfSemester');
+    dispatch(setStartOfSemester(storedStartOfSemester.date));
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function loadStartOfSemesterAndSaveFromApi(dispatch) {
+  try {
+    const apiStartOfSemesterResponse = await axios.get('https://sibsutis-schedule-api-nine.vercel.app/start-of-semester', {
+      headers: {
+        'x-api-key': '0GpVzFuguPr62pT6c4CCifHP4KvxD4zgMznTyH6lRq6d2yrfZe7rS3LTmzTVKTho',
+      },
+    });
+
+    await saveDataInStorage('startOfSemester', { date: apiStartOfSemesterResponse.data.date });
+    dispatch(setStartOfSemester(apiStartOfSemesterResponse.data.date));
+  } catch (error) {
+    console.log(error.message);
+  }
+}
