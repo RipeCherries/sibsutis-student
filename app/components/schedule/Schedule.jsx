962f509f38fd7b829955de1e2bd7d438e23a3e72
Alexey Egoshin <alexeyegoshin0403@mail.ru>
2023-10-15 21:31:21 +0700

    Fix eslint errors
    Reduced the length of the code lines and added data types props

--- app/components/schedule/Schedule.jsx
+++ app/components/schedule/Schedule.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { ScrollView } from 'react-native';
 import Lesson from '../lesson/Lesson';
 
@@ -6,12 +7,35 @@ function Schedule({ lessons, selectedD }) {
   return (
     <ScrollView>
       {
-                lessons.map((item) => (
-                  <Lesson key={item._id} time={item.time} name={item.name} teacher={item.teacher} room={item.room} selectedD={selectedD} />
-                ))
-            }
+        lessons.map((item) => (
+          <Lesson
+            key={item._id}
+            time={item.time}
+            name={item.name}
+            teacher={item.teacher}
+            room={item.room}
+            selectedD={selectedD}
+          />
+        ))
+      }
     </ScrollView>
   );
 }
+
+Schedule.propTypes = {
+  lessons: PropTypes.arrayOf(PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+    time: PropTypes.shape({
+      startHours: PropTypes.number.isRequired,
+      startMinutes: PropTypes.number.isRequired,
+      endHours: PropTypes.number.isRequired,
+      endMinutes: PropTypes.number.isRequired,
+    }).isRequired,
+    name: PropTypes.string.isRequired,
+    teacher: PropTypes.string.isRequired,
+    room: PropTypes.string.isRequired,
+  })).isRequired,
+  selectedD: PropTypes.instanceOf(Date).isRequired,
+};
 
 export default Schedule;
