1804ae8b2601d020800eecb6a8732b79b7ec924d
Alexey Egoshin <alexeyegoshin0403@mail.ru>
2023-10-15 20:51:30 +0700

    Fix eslint errors
    Reduced the length of the strings, added data types for props and changed the key to a more unique one when rendering the array

--- app/components/calendar-page/CalendarPage.jsx
+++ app/components/calendar-page/CalendarPage.jsx
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 import { Pressable, Text, View } from 'react-native';
 
 import CalendarPageStyles from './CalendarPage.styles';
@@ -23,31 +24,47 @@ function CalendarPage({ week, selectedDate, onDateChange }) {
 
   return (
     <View style={CalendarPageStyles.container}>
-      {
-                week.map((item, index) => (
-                  <Pressable
-                    style={[
-                      CalendarPageStyles.day,
-                      item.obj.getDate() === selectedDate.getDate() ? [CalendarPageStyles.selectedDay, { borderColor: themeColors.primary }] : '',
-                    ]}
-                    onPress={() => onDateChange(item.obj)}
-                    key={index}
-                  >
-                    <Text
-                      style={[CalendarPageStyles.dayName, { color: currentDate.getDate() === item.day ? themeColors.primary : themeColors.textShade }]}
-                    >
-                      {item.dayName}
-                    </Text>
-                    <Text
-                      style={[CalendarPageStyles.dayValue, { color: currentDate.getDate() === item.day ? themeColors.primary : themeColors.text }]}
-                    >
-                      {item.day}
-                    </Text>
-                  </Pressable>
-                ))
-            }
+      {week.map((item) => (
+        <Pressable
+          style={[
+            CalendarPageStyles.day,
+            item.obj.getDate() === selectedDate.getDate() ? [CalendarPageStyles.selectedDay,
+              { borderColor: themeColors.primary },
+            ] : '',
+          ]}
+          onPress={() => onDateChange(item.obj)}
+          key={item.day}
+        >
+          <Text
+            style={[
+              CalendarPageStyles.dayName,
+              { color: currentDate.getDate() === item.day ? themeColors.primary : themeColors.textShade },
+            ]}
+          >
+            {item.dayName}
+          </Text>
+          <Text
+            style={[
+              CalendarPageStyles.dayValue,
+              { color: currentDate.getDate() === item.day ? themeColors.primary : themeColors.text },
+            ]}
+          >
+            {item.day}
+          </Text>
+        </Pressable>
+      ))}
     </View>
   );
 }
+
+CalendarPage.propTypes = {
+  week: PropTypes.arrayOf(PropTypes.shape({
+    dayName: PropTypes.string.isRequired,
+    day: PropTypes.number.isRequired,
+    obj: PropTypes.instanceOf(Date).isRequired,
+  })).isRequired,
+  selectedDate: PropTypes.instanceOf(Date).isRequired,
+  onDateChange: PropTypes.func.isRequired,
+};
 
 export default CalendarPage;
