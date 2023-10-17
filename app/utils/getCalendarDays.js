9c8fd5ff7a6d3463267a505d623e16b0576d60fc
Alexey Egoshin <alexeyegoshin0403@mail.ru>
2023-10-17 09:06:42 +0700

    Replaced the ++ operators and moved the function implementation up

--- app/utils/getCalendarDays.js
+++ app/utils/getCalendarDays.js
@@ -1,3 +1,8 @@
+function getDayTitle(dayIndex) {
+  const daysOfWeek = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'];
+  return daysOfWeek[dayIndex];
+}
+
 export function getCalendarDays(date) {
   const daysUntilMonday = (date.getDay() === 0 ? 7 : date.getDay()) - 1;
   date.setDate(date.getDate() - daysUntilMonday - 7);
@@ -7,10 +12,10 @@ export function getCalendarDays(date) {
 
   const result = [];
 
-  for (let i = 0; i < countWeeks; ++i) {
+  for (let i = 0; i < countWeeks; i += 1) {
     const week = [];
 
-    for (let j = 0; j < daysInWeek; ++j) {
+    for (let j = 0; j < daysInWeek; j += 1) {
       const tmp = {};
       tmp.dayName = getDayTitle(j);
       tmp.day = date.getDate();
@@ -24,9 +29,4 @@ export function getCalendarDays(date) {
   }
 
   return result;
-}
-
-function getDayTitle(dayIndex) {
-  const daysOfWeek = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'];
-  return daysOfWeek[dayIndex];
 }
